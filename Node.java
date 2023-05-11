c38a9c0a3cade57a8ae0678cfb23c32f03706f94
KhaledEkramy <117099628+KhaledEkramy@users.noreply.github.com>
2023-05-11 12:26:10 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,33 @@
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafiles.binarysearchtree;
+
+/**
+ *
+ * @author Khaled
+ */
+public class Node {
+    Student student ;
+    Node left ;
+    Node right ;
+    
+    public Node (String name , long id , float grade) {
+    	student = new Student(name , id , grade ) ;
+    }
+    
+    public long getID() {
+    	return student.getID() ;
+    }
+    
+    public void display() {
+    	System.out.println(student.toString());
+    }
+
+	@Override
+	public String toString() {
+		return "[student=" + student + "]";
+	}
+    
+    
+}
